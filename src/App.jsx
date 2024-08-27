00a9405d9c8031650d1bb9393ccf862b3f0c3dd8
Andreas Gruber <andreas.gruber1@iu-study.org>
2024-08-27 21:09:56 +0200

    Test zur Vorführung

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function App() {
-  const hello = "Hello World von der IU Projektgruppe Software Engineering! Versionsverwaltung mit CI/CD läuft.";
+  const hello = "Hello World von der IU Projektgruppe Software Engineering! Versionsverwaltung mit CI/CD läuft. Hallo nochmal.";
 
   return (
     <Container fluid>
