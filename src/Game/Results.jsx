5800ba7d409a8e2720f96e266098d71383e518ac
Andreas Gruber <andreas.gruber1@iu-study.org>
2024-10-02 17:54:10 +0200

    Results page

--- src/Game/Results.jsx
+++ src/Game/Results.jsx
@@ -18,6 +18,7 @@ export default function Results() {
   const navigate = useNavigate();
   const { activeCourse } = useActiveCourse();
 
+
   const exitPrivateLobby = useCallback(
     (privateLobbyId) => {
       const defaultStatuses = {
@@ -44,15 +45,13 @@ export default function Results() {
 
   const handleExit = useCallback(async () => {
     try {
-      if (gameData?.game_mode !== "single") {
-        exitPrivateLobby(gameId);
-      }
+      exitPrivateLobby(gameId);
       navigate('/');
     } catch (err) {
       console.error("Error exiting game:", err);
       setError("Fehler beim Verlassen des Spiels.");
     }
-  }, [exitPrivateLobby, gameData?.game_mode, gameId, navigate]);
+  }, [exitPrivateLobby, gameId, navigate]);
 
   useEffect(() => {
     async function fetchGameData() {
@@ -96,137 +95,195 @@ export default function Results() {
     return <Typography>Spieldaten konnten nicht geladen werden.</Typography>;
   }
 
-  const isPlayer1 = auth.currentUser.uid === gameData.player1.uid;
-  const currentPlayerAnswers = isPlayer1 ? gameData.answers_player1 : gameData.answers_player2;
-  const otherPlayerAnswers = isPlayer1 ? gameData.answers_player2 : gameData.answers_player1;
+  const { questions, answers_player1, answers_player2, game_mode, player1 } = gameData;
 
-  const correctAnswers = gameData?.questions.reduce((count, question, index) => {
-    return count + (currentPlayerAnswers[index] === question.correct_answer ? 1 : 0);
+  const isPlayer1 = auth.currentUser.uid === player1.uid;
+  const currentPlayerAnswers = isPlayer1 ? answers_player1 : answers_player2;
+  const otherPlayerAnswers = isPlayer1 ? answers_player2 : answers_player1;
+  const gameMode = game_mode;
+
+  const correctAnswers = questions.reduce((count, question, index) => {
+    return count + (currentPlayerAnswers[index].toLowerCase() === question.correct_answer.toLowerCase() ? 1 : 0);
   }, 0);
 
-  const correctAnswersOpponent = gameData?.questions.reduce((count, question, index) => {
-    if (gameData?.game_mode != "single") {
-      return count + (otherPlayerAnswers[index] === question.correct_answer ? 1 : 0);
+  const correctAnswersOpponent = questions.reduce((count, question, index) => {
+    if (gameMode != "single") {
+      return count + (otherPlayerAnswers[index].toLowerCase() === question.correct_answer.toLowerCase() ? 1 : 0);
     } else {
       return 0
     }
   }, 0);
 
-  return (
-    <Paper elevation={3} sx={{ p: 3, maxWidth: 800, mx: 'auto', my: 4 }}>
-      <Grid size={{ xs: 12 }} sx={{ mb: 3 }} container spacing={2}>
-        <Grid size={{ xs: 12 }} >
-          <Typography
-            variant="h5"
-            noWrap
-            gutterBottom
-            className='iuHeadline1'
-            sx={{
-              fontWeight: 700
-            }}
-          >
-            DEINE ERGEBNISSE
-          </Typography>
+  console.log("correctAnswers", correctAnswers);
+  console.log("questions", questions);
+  console.log("currentPlayerAnswers", currentPlayerAnswers);
+
+  if (gameData) {
+    return (
+      <Paper elevation={3} sx={{ p: 3, maxWidth: 800, mx: 'auto', my: 4 }}>
+        <Grid size={{ xs: 12 }} sx={{ mb: 3 }} container spacing={2}>
+        {gameMode === 'single' && (
+            <Grid size={{ xs: 12 }} >
+              <Typography
+              variant="h5"
+              noWrap
+              gutterBottom
+              className='iuHeadline1'
+              sx={{
+                fontWeight: 700
+              }}
+            >
+              DEIN ERGEBNIS
+            </Typography>
+            </Grid>
+          )}
+          {(gameMode === 'competition' && correctAnswers > correctAnswersOpponent) && (
+            <Grid size={{ xs: 12 }} >
+              <Typography
+              variant="h5"
+              noWrap
+              gutterBottom
+              className='iuHeadline2'
+              sx={{
+                fontWeight: 700
+              }}
+            >
+              GLÜCKWUNSCH, GEWONNEN!
+            </Typography>
+            </Grid>
+          )}
+          {(gameMode === 'competition' && correctAnswers < correctAnswersOpponent) && (
+            <Grid size={{ xs: 12 }} >
+              <Typography
+              variant="h5"
+              noWrap
+              gutterBottom
+              className='iuHeadline1'
+              sx={{
+                fontWeight: 700
+              }}
+            >
+              DER LERNERFOLG ZÄHLT!
+            </Typography>
+            </Grid>
+          )}
+          {gameMode !== 'competition' && (
+            <>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }} >
+                <Typography variant="h6">
+                  Korrekte Antworten
+                </Typography>
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 9 }} >
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  {correctAnswers} von {questions.length}
+                </Typography>
+              </Grid>
+            </>
+          )}
+          {gameMode === 'competition' && (
+            <>
+              <Grid size={{ xs: 12 }} >
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  Korrekte Antworten
+                </Typography>
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }} >
+                <Typography variant="h6">
+                  Du:
+                </Typography>
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 9 }} >
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  {correctAnswers} von {questions.length}
+                </Typography>
+              </Grid>
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <Typography variant="h6">
+                  Gegenspieler:
+                </Typography>
+              </Grid><Grid size={{ xs: 12, sm: 6, md: 9 }}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  {correctAnswersOpponent} von {questions.length}
+                </Typography>
+              </Grid>
+            </>
+          )}
+          <Grid>
+            <Typography variant="h6">
+              Das Spiel dauerte {minutes} {(minutes == 1) ? "Minute" : "Minuten"} {seconds} {(seconds == 1) ? "Sekunde" : "Sekunden"}.
+            </Typography>
+          </Grid>
         </Grid>
         <Grid size={{ xs: 12 }} >
           <Button variant="contained" color="secondary" onClick={handleExit}>
             Spiel verlassen
           </Button>
         </Grid>
-        <Grid size={{ xs: 12 }} >
-          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-            Korrekte Antworten
-          </Typography>
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 3 }} >
-          <Typography variant="h6">
-            Du:
-          </Typography>
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6, md: 9 }} >
-          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-            {correctAnswers} von {gameData?.questions.length}
-          </Typography>
-        </Grid>
-        {gameData?.game_mode === 'competition' && (
-          <>
-            <Grid size={{ xs: 12, sm: 6, md: 3 }}>
-              <Typography variant="h6">
-                Gegenspieler:
-              </Typography>
-            </Grid><Grid size={{ xs: 12, sm: 6, md: 9 }}>
-              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                {correctAnswersOpponent} von {gameData?.questions.length}
-              </Typography>
-            </Grid>
-          </>
-        )}
-        <Grid>
-          <Typography variant="h6">
-            Das Spiel dauerte {minutes} {(minutes == 1) ? "Minute" : "Minuten"} {seconds} {(seconds == 1) ? "Sekunde" : "Sekunden"}.
-          </Typography>
-        </Grid>
-      </Grid>
 
-      <List>
-        {gameData?.questions.map((question, index) => (
-          <>
-            <ListItem alignItems="flex-start">
-              <ListItemText
-                primary={
-                  <Typography variant="h6">
-                    Frage {index + 1}: {gameData?.questions.question_text}
-                  </Typography>
-                }
-                secondary={
-                  <Grid container spacing={2} mt={1}>
-                    {['A', 'B', 'C', 'D'].map((option) => (
-                      <Grid size={{ xs: 12, sm: 6 }} key={option}>
-                        <Box
-                          sx={{
-                            p: 1,
-                            border: '1px solid',
-                            borderColor: 'grey.300',
-                            borderRadius: 1,
-                            backgroundColor:
-                              option === question.correct_answer.toUpperCase()
-                                ? 'success.light'
-                                : option === currentPlayerAnswers[index].toUpperCase()
-                                  ? 'error.light'
-                                  : 'inherit',
-                          }}
-                        >
-                          <Typography
-                            variant="body2"
-                            color={
-                              option === question.correct_answer.toUpperCase() || option === currentPlayerAnswers[index].toUpperCase()
-                                ? 'white'
-                                : 'inherit'
-                            }
+        <List>
+          {gameData?.questions.map((question, index) => (
+            <>
+              <ListItem alignItems="flex-start">
+                <ListItemText
+                  primary={
+                    <Typography variant="h6">
+                      Frage {index + 1}: {question.question_text}
+                    </Typography>
+                  }
+                  secondary={
+                    <Grid container spacing={2} mt={1}>
+                      {['A', 'B', 'C', 'D'].map((option) => (
+                        <Grid size={{ xs: 12, sm: 6 }} key={option}>
+                          <Box
+                            sx={{
+                              p: 1,
+                              border: '1px solid',
+                              borderColor: 'grey.300',
+                              borderRadius: 1,
+                              backgroundColor:
+                                option === question.correct_answer.toUpperCase()
+                                  ? 'success.light'
+                                  : option === currentPlayerAnswers[index].toUpperCase()
+                                    ? 'error.light'
+                                    : 'inherit',
+                            }}
                           >
-                            {option}: {question[`answer_${option.toLowerCase()}`]}
-                          </Typography>
-                        </Box>
-                      </Grid>
-                    ))}
-                  </Grid>
-                }
-              />
-            </ListItem>
-            {gameData?.game_mode === 'competition' && (
-              <Box ml={4} mb={2}>
-                <Typography variant="body2">
-                  Deine Antwort: {currentPlayerAnswers[index].toUpperCase() || 'keine Antwort'}
-                </Typography>
-                <Typography variant="body2">
-                  Antwort deines Gegners: {otherPlayerAnswers[index].toUpperCase() || 'keine Antwort'}
-                </Typography>
-              </Box>
-            )}
-            {index < gameData?.questions.length - 1 && <Divider component="li" />}
-          </>
-        ))}
-      </List>
-    </Paper>
-  );
+                            <Typography
+                              variant="body2"
+                              color={
+                                option === question.correct_answer.toUpperCase() || option === currentPlayerAnswers[index].toUpperCase()
+                                  ? 'white'
+                                  : 'inherit'
+                              }
+                            >
+                              {option}: {question[`answer_${option.toLowerCase()}`]}
+                            </Typography>
+                          </Box>
+                        </Grid>
+                      ))}
+                    </Grid>
+                  }
+                />
+              </ListItem>
+              {gameData?.game_mode === 'competition' && (
+                <Box ml={4} mb={2}>
+                  <Typography variant="body2">
+                    Richtige Antwort: {question.correct_answer.toUpperCase()}
+                  </Typography>
+                  <Typography variant="body2">
+                    Deine Antwort: {currentPlayerAnswers[index].toUpperCase() || 'keine Antwort'}
+                  </Typography>
+                  <Typography variant="body2">
+                    Antwort deines Gegners: {otherPlayerAnswers[index].toUpperCase() || 'keine Antwort'}
+                  </Typography>
+                </Box>
+              )}
+              {index < questions.length - 1 && <Divider sx={{ my: 3 }} />}
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  }
 }
