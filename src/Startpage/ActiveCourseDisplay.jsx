719681e104c3e75c3813c0a998428c26be5cff82
Andreas Gruber <andreas.gruber1@iu-study.org>
2024-10-15 20:04:17 +0200

    bug fix aktiver Kurs

--- src/Startpage/ActiveCourseDisplay.jsx
+++ src/Startpage/ActiveCourseDisplay.jsx
@@ -13,30 +13,32 @@ export function ActiveCourseDisplay({ activeCourse, loading, handleChangeCourse 
           <Typography variant="h5" component="div">
             Kein Kurs ausgewählt
           </Typography>
-          <CourseSelector handleChangeCourse={handleChangeCourse} />
+          <CourseSelector activeCourse={{ image_path: "", long_name: "", short_name: "", id: "" }} handleChangeCourse={handleChangeCourse} />
         </CardContent>
       </Card>
     );
   }
 
-  return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="200"
-        width="200"
-        image={activeCourse.imageUrl}
-        alt={activeCourse.long_name}
-      />
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {activeCourse.long_name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {activeCourse.short_name}
-        </Typography>
-        <CourseSelector activeCourse={activeCourse} handleChangeCourse={handleChangeCourse} />
-      </CardContent>
-    </Card>
-  );
+  else if (activeCourse && activeCourse.imageUrl && activeCourse.long_name && activeCourse.short_name) {
+    return (
+      <Card>
+        <CardMedia
+          component="img"
+          height="200"
+          width="200"
+          image={activeCourse.imageUrl}
+          alt={activeCourse.long_name}
+        />
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {activeCourse.long_name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {activeCourse.short_name}
+          </Typography>
+          <CourseSelector activeCourse={activeCourse} handleChangeCourse={handleChangeCourse} />
+        </CardContent>
+      </Card>
+    );
+  }
 }
